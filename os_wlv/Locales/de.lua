c57138dcb364b4921e35ecd87d05ae4c773b3e53
Noel <108223454+Test1calCutter@users.noreply.github.com>
2023-07-05 16:27:21 -0700

    Add files via upload

---
+++ os_wlv/Locales/de.lua
@@ -0,0 +1,5 @@
+Locales = {}
+
+Locales['de'] = {
+    ['warning'] = 'Dieses Auto ist auf der Whitelist. Du wirst in 5 Sekunden herausgeworfen.',
+}
---
+++ os_wlv/Locales/en.lua
@@ -0,0 +1,3 @@
+Locales['en'] = {
+    ['warning'] = 'This car is whitelisted. You will be kicked out in 5 seconds.',
+}
